ed2e3214330ff097422fbe20335e5b2bed9a5464
Fábio Ceolin <fabioceolin98@gmail.com>
2021-05-06 22:38:06 -0300

    Added image login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-import { GetServerSideProps } from "next";
 import { FormEvent, useContext, useState } from "react";
+import Lottie from "react-lottie";
 
+import animationData from "../assets/smartHome.json";
+
+import { withSSRGuest } from "../utils/withSSRGuest";
 import { AuthContext } from "../contexts/AuthContext";
 
 import styles from "./home.module.scss";
-import { withSSRGuest } from "../utils/withSSRGuest";
 
 export default function Home() {
   const [email, setEmail] = useState("");
@@ -23,19 +25,34 @@ export default function Home() {
     await signIn(data);
   }
   return (
-    <form onSubmit={handleSubmit} className={styles.container}>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Entrar</button>
-    </form>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.contentForm}>
+          <form onSubmit={handleSubmit} className={styles.loginForm}>
+            <input
+              placeholder="Email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              placeholder="Senha"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Entrar</button>
+          </form>
+        </div>
+        <div className={styles.image}>
+          <Lottie
+            options={{ loop: true, animationData: animationData }}
+            height={600}
+            width={600}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
 
